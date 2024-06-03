7ed9b3bb2eb06fc9a46f34e0a1749a0e91bbd2d4
Kiyeon Nam <nky3919@gmail.com>
2024-06-03 16:52:01 +0900

    feat: 로그인 페이지 레이아웃 추가

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
+
 function Login(props) {
   return (
     <main className="login">
-
+      <div class="container">
+        <img src="./logo.png"/>
+        <button type="button">
+          <img src="#" alt="login_button" />
+        </button>
+        <Link to="/private">개인정보처리방침</Link>
+      </div>
     </main>
   )
 }
