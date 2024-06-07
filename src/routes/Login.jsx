6263cf34216dbed9cda4b024ba9e3e021acec172
Kiyeon Nam <nky3919@gmail.com>
2024-06-07 19:17:13 +0900

    feat: 스타일 추가

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,18 +1,12 @@
 import './Login.css';
 import { Link } from "react-router-dom";
-import logo from '../assets/image/logo.png';
-import login_button from '../assets/image/login_button.png';
 
 function Login(props) {
   return (
     <main className="login">
-      <div className="container">
-        <img src={logo} alt="logo" />
-        <button type="button" className="login-button">
-          <img src={login_button} alt="login_button" />
-        </button>
-        <Link to="/private">개인정보처리방침</Link>
-      </div>
+      <div className="login-logo" />
+      <button className="login-login-button" type="button" />
+      <Link className="login-policy" to="/policy">개인정보처리방침</Link>
     </main>
   )
 }
