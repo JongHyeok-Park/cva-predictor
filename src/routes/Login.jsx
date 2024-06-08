c90df5aadc9a0c9d641b7f9ca91038e83197c90d
Kiyeon Nam <nky3919@gmail.com>
2024-06-08 14:47:16 +0900

    fix: 이미지 수정

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,12 +1,16 @@
 import './Login.css';
 import { Link } from "react-router-dom";
+import logoImage from '../assets/image/logo.png';
+import loginBtnImage from '../assets/image/login_button.png';
 
 function Login(props) {
   return (
     <main className="login">
-      <div className="login-logo" />
+      <img className="login-logo" src={logoImage} alt="logo" />
       <div className="login-button-container">
-        <button className="login-button" type="button" />
+        <button className="login-button" type="button">
+          <img src={loginBtnImage} alt="login_button" />
+        </button>
         <Link className="login-policy" to="/policy">개인정보처리방침</Link>
       </div>
     </main>
