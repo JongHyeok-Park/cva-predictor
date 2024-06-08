5bb22dd236cb40522e4227be74983c4253767611
Kiyeon Nam <nky3919@gmail.com>
2024-06-08 12:57:19 +0900

    fix: 스타일 수정

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -5,8 +5,10 @@ function Login(props) {
   return (
     <main className="login">
       <div className="login-logo" />
-      <button className="login-login-button" type="button" />
-      <Link className="login-policy" to="/policy">개인정보처리방침</Link>
+      <div className="login-button-container">
+        <button className="login-button" type="button" />
+        <Link className="login-policy" to="/policy">개인정보처리방침</Link>
+      </div>
     </main>
   )
 }
