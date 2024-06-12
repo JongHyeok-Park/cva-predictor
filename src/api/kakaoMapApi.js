02757f28a607c0fdfb9d2c282525fd20f9c25113
Jonghyeok <jhpmuscle@gmail.com>
2024-06-12 23:54:31 +0900

    feat: redirect to kakao map

--- src/api/kakaoMapApi.js
+++ src/api/kakaoMapApi.js
@@ -13,4 +13,19 @@ const getAddressApi = async (keyword) => {
   return res.json();
 }
 
-export { getAddressApi };
+const getPlaceInfoApi = async (keyword) => {
+  const res = await fetch(`https://dapi.kakao.com/v2/local/search/keyword.json?query=${keyword}`, {
+    headers: {
+      authorization: `KakaoAK ${process.env.REACT_APP_REST_API_KEY}`
+    }
+  });
+
+  if (!res.ok) {
+    const message = await res.text();
+    throw new Error(message);
+  };
+
+  return res.json();
+}
+
+export { getAddressApi, getPlaceInfoApi };
--- src/components/post/HospitalItem.jsx
+++ src/components/post/HospitalItem.jsx
@@ -1,7 +1,24 @@
 import { Link } from "react-router-dom";
 import './HospitalItem.css';
+import { useEffect, useState } from "react";
+import { getPlaceInfoApi } from "../../api/kakaoMapApi";
 
 function HospitalItem(props) {
+  const [lat, lng] = [128.604702568416, 35.8662511316347]
+  const name = '경북대학교 병원';
+  const [placeId, setPlaceId] = useState();
+
+  useEffect(() => {
+    getPlaceInfoApi(name)
+      .then((data) => {
+        console.log(data.documents);
+        setPlaceId(data.documents[0]?.id);
+      })
+      .catch((error) => {
+        console.error(error.message);
+      })
+  })
+
   return (
     <article className="hospital-item">
       <div className="hospital-item-type-icon">
@@ -9,12 +26,13 @@ function HospitalItem(props) {
       </div>
       <div className="hospital-item-info">
         <div className="hospital-item-info-main">
-          <h4 className="hospital-name">경북대학교 병원</h4>
+          <h4 className="hospital-name">{name}</h4>
           <span className="hospital-item-distance">230m</span>
         </div>
         <div className="hospital-item-info-secondary">
           <p className="hospital-item-address">대구광역시 중구 동덕로 130</p>
-          <Link className="hospital-item-map-link">지도 보기 &gt;</Link>
+          <a className="hospital-item-map-link" href={`https://map.kakao.com/link/map/${placeId}`} target="_blank">지도 보기 &gt;</a>
+          {/* <Link className="hospital-item-map-link" to={`/map?lat=${128.604702568416}&lng=${35.8662511316347}`}>지도 보기 &gt;</Link> */}
         </div>
       </div>
     </article>
