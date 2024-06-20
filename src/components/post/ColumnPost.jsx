71d3911158dec148e34d92ab176e5543a12f2caa
Jonghyeok <jhpmuscle@gmail.com>
2024-06-21 02:54:49 +0900

    feat: column recommendation

--- src/components/post/ColumnPost.jsx
+++ src/components/post/ColumnPost.jsx
@@ -12,11 +12,10 @@ function ColumnPost(props) {
         </figure>
         <div className="column-post-info">
           <h4 className="column-post-info-title">
-            꾸준한 유산소 운동으로 심장을 건강하게!
+            {props.title}
           </h4>
           <p className="column-post-info-content">
-            유산소 운동은 무산소 운동의 반대말로 뜻은 에너지를 산소 대사를 통해 얻는 지속적인 힘을 내야 하는 운동을 말한다.
-            즉, 지방과 글리코겐을 완전연소 시킬 수 있는 강도로 행해지는 운동을 의미한다. 일반적으로 많은 산소를...
+            {props.content}
           </p>
         </div>
       </Link>
